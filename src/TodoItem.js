2398baa31f88e08bcfd8741cdace0e4c377ccd3d
Manan Surana <59257437+suranamanan@users.noreply.github.com>
2020-06-19 11:20:06 -0400

    final

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,20 @@
 import React from "react"
 
 function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
     return (
         <div className="todo-item">
             <input
                 type="checkbox"
                 checked={props.item.completed}
-                onChange={() => console.log("Changed!")}
+                onChange={() => props.handleChange(props.item.id)}
             />
-            <p>{props.item.text}</p>
+            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
         </div>
     )
 }
